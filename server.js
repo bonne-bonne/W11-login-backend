101c88cde4afc0ede20be003115be69fe6ceb1b7
Bonnie Wall <111819881+bonne-bonne@users.noreply.github.com>
2024-04-15 18:42:17 +1200

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,51 @@
-// Module Imports
+// ======== Package Imports ========= //
 const express = require("express");
 const mysql = require("mysql2");
-const cors = require("cors");
 require("dotenv").config();
+const cors = require("cors");
 
 // Enable express
 const app = express();
 
-// Middlewares
+// ========== Middleware ============= //
 app.use(cors());
 
-// SQL connection
+//Create the connection to the database using pool
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASS,
-  database: process.env.MYSQL_DATABASE,
+  password: process.env.MYSQL_PASSWORD, // your password for root
+  database: process.env.MYSQL_DATABASE, // database name
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
-// ===============================//
-// API Endpoint
-app.get("/", (req, res) => {
-  res.send("Hey backend is running yoooooo~😉");
-});
 
-// API: USER Endpoint
-// users Endpoint which retrieves the data from team_mate table
-app.get("/users", (req, res) => {
-  pool.query(`SELECT name, images FROM team_mate;`, (err, result) => {
-    if (err) console.log(err);
-    res.send(result);
+//============ API ENDPOINTS ==============//
+app.get("/", (req, res) => {
+  console.log("/ endpoint was hit 🎯");
+
+  pool.query(`SELECT * FROM team_mate`, (err, result) => {
+    if (err) {
+      console.log("Database error:", err);
+      return res.status(500).json({errorMessage: "An error occurred while fetching data from the database."});
+    } else {
+      res.send(result);
+    }
   });
 });
 
-// Port
-const port = process.env.PORT;
+//============= PORT =================//
+const PORT = process.env.PORT;
 
 app
-  .listen(port, () => {
-    console.log(`It's working yo, at http://localhost:${port}`);
+  .listen(PORT, () => {
+    console.log(`Server is alive on http://localhost:${PORT}`);
   })
-  .on("error", (error) => console.log(error));
+  .on("error", (error) => {
+    if (error.code === "EADDRINUSE") {
+      console.log("Port is already in use");
+    } else {
+      console.log("Server Error", error);
+    }
+  });
